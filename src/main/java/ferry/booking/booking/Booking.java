5e970d59eff59297cb5384376d6d75066800d617
Alvaro Garcia <derecerca@gmail.com>
2017-05-13 01:45:17 +0200

    Refactor: prefer making fields final

--- src/main/java/ferry/booking/booking/Booking.java
+++ src/main/java/ferry/booking/booking/Booking.java
@@ -2,8 +2,8 @@ package ferry.booking.booking;
 
 public class Booking {
 
-    public int journeyId;
-    public int passengers;
+    public final int journeyId;
+    public final int passengers;
 
     public Booking(int journeyId, int passengers) {
         this.journeyId = journeyId;
